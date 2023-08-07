1fc45947ad2cdc20d895f818713bc025eb6d068f
Leonardo Metzger <leonardo.metzger@outlook.com>
2023-08-07 07:56:23 -0300

    feat: add testing files for merging communities

--- datastructures/merging_communities_test.go
+++ datastructures/merging_communities_test.go
@@ -1,6 +1,11 @@
 package datastructures
 
 import (
+	"bufio"
+	"io"
+	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -39,5 +44,93 @@ func TestMergingCommunities(t *testing.T) {
 
 	for _, test := range tests {
 		assert.Equal(t, test.result, MergingCommunities(test.n, test.commands, test.params))
+	}
+}
+
+func TestMergingCommunitiesFiles(t *testing.T) {
+	n, commands, params := getFunctionArgsFromFile("./tests/mc04.txt")
+	expectedResult := getResultFromFile("./tests/mc04-result.txt")
+	result := MergingCommunities(n, commands, params)
+
+	assert.Equal(t, expectedResult, result)
+}
+
+func getFunctionArgsFromFile(filePath string) (int32, []string, [][]int32) {
+	commands := []string{}
+	params := [][]int32{}
+
+	file, err := os.Open(filePath)
+	checkError(err)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	firstLine := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+	nTemp, err := strconv.ParseInt(firstLine[0], 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	qTemp, err := strconv.ParseInt(firstLine[1], 10, 64)
+	checkError(err)
+	q := int32(qTemp)
+
+	for i := 0; i < int(q); i++ {
+		line := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+		if line[0] == "" {
+			break
+		}
+
+		command := line[0]
+		commands = append(commands, command)
+
+		param1Temp, err := strconv.ParseInt(line[1], 10, 64)
+		checkError(err)
+		param1 := int32(param1Temp)
+
+		if command == "M" {
+			param2Temp, err := strconv.ParseInt(line[2], 10, 64)
+			checkError(err)
+			param2 := int32(param2Temp)
+			params = append(params, []int32{param1, param2})
+		} else {
+			params = append(params, []int32{param1})
+		}
+	}
+	return n, commands, params
+}
+
+func getResultFromFile(filePath string) []int32 {
+	file, err := os.Open(filePath)
+	checkError(err)
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+	var results []int32
+
+	for {
+		item := readLine(reader)
+		if item == "" {
+			break
+		}
+
+		intItem, err := strconv.ParseInt(item, 10, 64)
+		checkError(err)
+		results = append(results, int32(intItem))
+	}
+	return results
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, err := reader.ReadString('\n')
+
+	if err == io.EOF {
+		return ""
+	}
+	return strings.TrimRight(str, "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
